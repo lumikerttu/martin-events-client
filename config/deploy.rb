e794a37c8a683a859edb1e9aea3ecbfd6d41d689
Kerttu Lumi <kerttu.lumi@khk.ee>
2018-10-18 13:46:51 +0300

    db

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/srv/apps/kerttu"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
+append :linked_files, "config/master.key", "db/production.sqlite3"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
